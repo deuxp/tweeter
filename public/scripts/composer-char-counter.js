a6d32c0a6fde4bc4a7d7619906ec9f9169fdda5e
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-12-13 13:34:18 -0500

    implement counter functionality and look

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,27 @@
 
 $(document).ready(() => {
   const $tweetText = $('#tweet-text')
-  const chars = 140
 
-  
-  $tweetText.on('keypress', function(e) {
-    const $thang = $(this)
-    console.log($thang);
+  $tweetText.on('keyup', function() {
+    const $count = $(this).val().length; // <-- `this` is the element where the event is occuring
+    // const $counter = $('.counter')
+
+    // logs
+    // console.log(`this is the parent of "this":`);
+    const $counter = $(this).parents('form').children('.submission-area').children('.counter');
+    // console.log(a);
+    // console.log(`this is "this": `);
+    // console.log(this);
+
+    // change inner html of $counter
+    const charsLeft = Number(140) - Number($count)
+    if (charsLeft < 1) {
+      $counter.css('color', 'red')
+    } else {
+      $counter.css('color', '#4056A1')
+    }
+    $counter.html(charsLeft)
+
   })
 
 })
