6268adfe747dd5a0ad3f46f950328e21a69b3b7a
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-12-12 23:55:26 -0500

    stylize the structure of the form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// waits for DOM content to load before running callback
+$(document).on("ready", () => {
+  // find existing DOM elements with matching ids
+  const counter = $('.counter')
+
+  
+})
