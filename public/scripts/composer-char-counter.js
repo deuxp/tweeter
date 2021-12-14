48ecb2eadc02500605ff17ee8a191ddf7b0fd967
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-12-14 12:05:12 -0500

    specify behaviour for character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,13 @@
 $(document).ready(() => {
-  const $textBox = $('#tweet-text');
 
-  $textBox.on('keyup', function() {
+  $('#tweet-text').on('keyup', function() {
     const $count = $(this).val().length; // <-- `this` is the element where the event is occuring
     const $counter = $(this).parents('form').children('.submission-area').children('.counter');
 
     // change inner html of $counter
-    const charsLeft = Number(140) - Number($count);
+    const charsLeft = 140 - Number($count);
 
-    if (charsLeft < 1) {
+    if (charsLeft < 1) { // add css || remove css
       $counter.css('color', 'red');
     } else {
       $counter.css('color', '#4056A1');
