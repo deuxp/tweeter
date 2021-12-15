b62e3522881db3cb9163d0db10432a8e658fa5ed
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-12-15 16:49:25 -0500

    add error pop-up window

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,7 +6,6 @@
 
 
 $(document).ready(() => {
-
   // input  -> JSON object tweet
   // output -> HTML dom structure --> using $() and template literals for the tags || text --> appending to the article, which has CSS rules already applied
   
@@ -15,7 +14,10 @@ $(document).ready(() => {
     const { user, content, created_at } = tweet;
     const { name, avatars, handle } = user;
     const { text } = content;
-    const ts = timeago.format(created_at)
+    const timeStamp = timeago.format(created_at)
+
+    // XSS prevention
+    const $safeText = $('<div>').text(text).html()
     
     const $article = $(`
       <article id="tweet">
@@ -29,11 +31,11 @@ $(document).ready(() => {
           <div class="handle">${handle}</div>
         </header>
         
-        <div class="message">${text}</div>
+        <div class="message">${$safeText}</div>
 
         <footer>
 
-          <div class="time">${ts}</div>
+          <div class="time">${timeStamp}</div>
 
           <div class="likes">
             <div class="flag"><i class="fas fa-flag"></i></div>
@@ -57,23 +59,41 @@ $(document).ready(() => {
   }
 
 
-  // event listener for submit form
+  /** Helper Function: error pop-up window
+   * ----- ----- ----- ----- ----- ----- ----- ----- 
+   * @param {string} msg ->  display error message
+   * @param {string} id ->  id=" ... " element id
+   * @param {string} sibling -> insert created DOM element after this sibling
+   * Behaviour: The purpose of this function is to construct the 
+   * DOM element for the Error pop-up window. And insert itself next to a sibling
+   */
+  const errorBubble = (msg, id, sibling) => {
+    const $msg = $(`<div id="${id}">⚠️ - - ${msg} - - ⚠️</div>`)
 
+    // create DOM element
+    $msg.insertAfter(`${sibling}`)
+    // animate box
+    $(`#${id}`).hide().slideDown({duration: 'fast'})
+    setTimeout(()=>{$(`#${id}`).hide('600')}, 3000)
+  };
+
+
+
+  // event listener for submit form
   $('.new-tweet form').submit(function(e) {
     e.preventDefault();
     const text = $(this['0']).val();
     const $text = $(this['0']).serialize();
+    const errMsg1 = 'You need to enter some text'
+    const errMsg2 = 'You have exceeded the character limit'
 
     // validation: empty string
     if (!text) {
-      alert('start chirping!!')
-
+      errorBubble(errMsg1, 'error-msg', '.new-tweet h2')
 
     } else if (text.length > 140) {
+      errorBubble(errMsg2, 'error-msg', '.new-tweet h2')
 
-      alert('Too much chirping > 140 characters!!')
-    
-      
     } else {
       $.post('/tweets', $text)
       .then(_res => {
@@ -85,6 +105,8 @@ $(document).ready(() => {
     }
   })
   
+
+  // when called it grabs the tweet array from the database; type: JSON
   const loadTweets = function() {
 
     $.get({
