5180db3294c93a8f16e8cfe216574c96621961ac
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-12-16 23:51:59 -0500

    factor error popup

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,100 +4,121 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
 $(document).ready(() => {
-
-  /**
+    /**
    * ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
    * @param {JSON} tweet -> the tweets JSON object database
    * @returns the DOM object representation of one tweet
    * ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
    */
-  const createTweetElement = (tweet) => {
-    const { user, content, created_at } = tweet;
-    const { name, avatars, handle } = user;
-    const { text } = content;
-    const timeStamp = timeago.format(created_at)
-
-    // XSS prevention
-    const $safeText = $('<div>').text(text).html()
-    
-    const $article = $(`
-      <article id="tweet">
-        <header>
-
-          <div class="profile">
-            <img class="icon" src="${avatars}" alt="avatar">
-            <div class="user">${name}</div>
-          </div>
-
-          <div class="handle">${handle}</div>
-        </header>
-        
-        <div class="message">${$safeText}</div>
-
-        <footer>
-
-          <div class="time">${timeStamp}</div>
-
-          <div class="likes">
-            <div class="flag"><i class="fas fa-flag"></i></div>
-            <div class="retweet"><i class="fas fa-retweet"></i></div>
-            <div class="like"><i class="fas fa-heart"></i></div>
-          </div>
+     const createTweetElement = (tweet) => {
+      const { user, content, created_at } = tweet;
+      const { name, avatars, handle } = user;
+      const { text } = content;
+      const timeStamp = timeago.format(created_at)
+  
+      // XSS prevention
+      const $safeText = $('<div>').text(text).html()
+      
+      const $article = $(`
+        <article id="tweet">
+          <header>
+  
+            <div class="profile">
+              <img class="icon" src="${avatars}" alt="avatar">
+              <div class="user">${name}</div>
+            </div>
+  
+            <div class="handle">${handle}</div>
+          </header>
           
-        </footer>
-        
-      </article>
-    `)
-    return $article;
-  };
+          <div class="message">${$safeText}</div>
+  
+          <footer>
+  
+            <div class="time">${timeStamp}</div>
+  
+            <div class="likes">
+              <div class="flag"><i class="fas fa-flag"></i></div>
+              <div class="retweet"><i class="fas fa-retweet"></i></div>
+              <div class="like"><i class="fas fa-heart"></i></div>
+            </div>
+            
+          </footer>
+          
+        </article>
+      `)
+      return $article;
+    };
+    
+  
+    /**
+     * ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+     * @param {JSON} tweets
+     * Behaviour: Leverage createTweetElement() to append
+     *            each tweet element to the DOM
+     * ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+     */
+    const renderTweets = (tweets) => {
+      tweets.forEach(tweet => {
+        const $tweet = createTweetElement(tweet);
+        $('.container').append($tweet)
+      });
+    }
+  
+  
+    /** 
+     * Behaviour: Requests the tweet array from the database; type: JSON
+     *            Then renders the tweets to the DOM using renderTweets()
+     */
+     const loadTweets = function() {
+      $.get({
+        method: 'GET',
+        dataType: 'json',
+        url: '/tweets',
+        success: tweets => {renderTweets(tweets)}
+      })
+      .catch(err => {
+        console.log(err.message);
+      })
+    };
+  
+  
+    /** Helper Function: Appends error pop-up window nad presents it
+     * ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+     * @param {string} msg -> display error message
+     * @param {string} id -> id="" element id
+     * @param {string} sibling -> insert created DOM element after this sibling
+     * Behaviour: The purpose of this function is to construct the 
+     * DOM element for the Error pop-up window. And insert itself after a sibling
+     * ----- ----- ----- ----- ----- ----- ----- ----- ---------- ----- ----- 
+     */
+    const errorBubble = (msg, id, sibling) => {
+      // prevent duplicates
+      $(`#${id}`).remove();
+
+      const $msg = $(`<div id="${id}">⚠️ - - ${msg} - - ⚠️</div>`)
+      // create DOM element
+      $msg.insertAfter(`${sibling}`)
+  
+      // animate pop-up
+      $(`#${id}`).hide().slideDown({duration: 'fast'})
+  
+      // hide box pop-up upon form submission 
+      $('.new-tweet form').submit(function(e) {
+        e.preventDefault();
+
+      })
+    };
   
 
-  /**
-   * ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
-   * @param {JSON} tweets
-   * Behaviour: Leverage createTweetElement() to append
-   *            each tweet element to the DOM
-   * ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
-   */
-  const renderTweets = (tweets) => {
-    tweets.forEach(tweet => {
-      const $tweet = createTweetElement(tweet);
-      $('.container').append($tweet)
-    });
-  }
+  ////////////////////////////////////
+  // event listener for submit form //
+  ////////////////////////////////////
 
-
-  /** Helper Function: error pop-up window
-   * ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
-   * @param {string} msg -> display error message
-   * @param {string} id -> id="" element id
-   * @param {string} sibling -> insert created DOM element after this sibling
-   * Behaviour: The purpose of this function is to construct the 
-   * DOM element for the Error pop-up window. And insert itself after a sibling
-   * ----- ----- ----- ----- ----- ----- ----- ----- ---------- ----- ----- 
-   */
-  const errorBubble = (msg, id, sibling) => {
-    const $msg = $(`<div id="${id}">⚠️ - - ${msg} - - ⚠️</div>`)
-
-    // create DOM element
-    $msg.insertAfter(`${sibling}`)
-    // animate pop-up
-    $(`#${id}`).hide().slideDown({duration: 'fast'})
-    // hide box pop-up upon form submission 
-    $('.new-tweet form').submit(function(e) {
-      e.preventDefault();
-      $(`#${id}`).hide('fast');
-    })
-
-  };
-
-
-  // event listener for submit form
   $('.new-tweet form').submit(function(e) {
-
     e.preventDefault();
+
     const text = $(this['0']).val();
     const $text = $(this['0']).serialize();
     const errMsg1 = 'You need to enter some text'
@@ -113,36 +134,25 @@ $(document).ready(() => {
     } else {
       $.post('/tweets', $text)
       .then(() => {
-        $('.container #tweet').remove() // without this clearing the HTML, loadTweets() will append duplicate lists to the DOM
-        loadTweets()
-      })
-      .then(() => {
-        $('#error-msg').hide('slow')
-      })
-      .then(() => {
-        // clear form
+        // hide the error box && clear form
+        $('#error-msg').hide('fast')
         $('.new-tweet form').trigger('reset');
+
+        // Guard: clear parent DOM element so loadTweets() does NOT append duplicate elements
+        $('.container #tweet').remove()
+        loadTweets()
+        
+        // reset counter
+        $('.submission-area .counter').html('140')
+
       })
     }
   })
   
 
+  ////////////////////////////////
+  // INITIAL LOAD OF ALL TWEETS //
+  ////////////////////////////////
 
-  /**
-   * Behaviour: Requests the tweet array from the database; type: JSON
-   */
-  const loadTweets = function() {
-    $.get({
-      method: 'GET',
-      dataType: 'json',
-      url: '/tweets',
-      success: tweets => {renderTweets(tweets)}
-    })
-    .catch(err => {
-      console.log(err.message);
-    })
-  };
-
-  // init page
   loadTweets();
 })
