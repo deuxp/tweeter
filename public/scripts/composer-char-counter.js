f143b76e23c56756c2bebe5172d9397085db7e13
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-12-16 23:49:01 -0500

    format layout

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,33 @@
+
 $(document).ready(() => {
 
-  $('#tweet-text').on('keyup', function() {
+  $('#tweet-text').on('input', function() {
     const $count = $(this).val().length; // <-- `this` is the element where the event is occuring
     const $counter = $(this).parents('form').children('.submission-area').children('.counter');
 
     // change inner html of $counter
-    const charsLeft = 140 - Number($count);
+    let charsLeft = 140 - Number($count);
 
-    if (charsLeft < 1) { // add css || remove css
+    // add css || remove css
+    if (charsLeft < 1) { 
       $counter.css('color', 'red');
     } else {
       $counter.css('color', '#4056A1');
     }
     
     $counter.html(charsLeft);
-
+    
   });
+  
+  
+      ///////////////////
+      // reset counter //
+      ///////////////////
+  $('.new-tweet form').submit(function(e) {
+      e.preventDefault();
+      let $counts = $(this).parents('form').children('.submission-area').children('.counter');
+
+      $counts.html('140')
+  })
 
 });
